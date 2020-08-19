e55972efe63465735a79937798e46538cba9cff3
Kurt Guardia <kurtguardia@gmail.com>
2020-08-19 13:37:54 +0200

    implemented context two different ways

--- src/components/Booklist.js
+++ src/components/Booklist.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import { ThemeContext } from '../contexts/ThemeContext';
 
 const Booklist = () => {
-  return <div></div>;
+  const { isLightTheme, light, dark } = React.useContext(ThemeContext);
+  const theme = isLightTheme.isLightTheme ? light : dark;
+
+  return (
+    <div
+      className='book-list'
+      style={{ background: theme.bg, color: theme.syntax }}
+    >
+      <ul>
+        <li style={{ background: theme.ui }}>the way of kings</li>
+        <li style={{ background: theme.ui }}>the name of the wind</li>
+        <li style={{ background: theme.ui }}>the final empire</li>
+      </ul>
+    </div>
+  );
 };
 
 export default Booklist;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import { ThemeContext } from '../contexts/ThemeContext';
 
 const Navbar = () => {
-  return <div></div>;
+  return (
+    <ThemeContext.Consumer>
+      {(context) => {
+        const { isLightTheme, light, dark } = context;
+        const theme = isLightTheme.isLightTheme ? light : dark;
+
+        return (
+          <nav style={{ background: theme.ui, color: theme.syntax }}>
+            <h1>Context App</h1>
+            <ul>
+              <li>Home</li>
+              <li>About</li>
+              <li>Contact</li>
+            </ul>
+          </nav>
+        );
+      }}
+    </ThemeContext.Consumer>
+  );
 };
 
 export default Navbar;
