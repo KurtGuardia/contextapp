ecdb86880d22ea35dd6e8e57c7d56df0e3e8f954
Kurt Guardia <kurtguardia@gmail.com>
2020-08-27 13:40:08 +0200

    implemented toggle theme

---
+++ src/components/ThemeToggle.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { ThemeContext } from '../contexts/ThemeContext';
+
+const ThemeToggle = () => {
+  const { isLightTheme } = React.useContext(ThemeContext);
+  console.log(isLightTheme);
+
+  return (
+    <button onClick={() => isLightTheme.toggleTheme()}>Toggle the theme</button>
+  );
+};
+
+export default ThemeToggle;
